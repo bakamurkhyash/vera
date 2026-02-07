5b69de2b53453dc614569af49020a7df7ce5328f
bakamurkhyash <bakamurkhyash@gmail.com>
2026-02-07 20:36:24 +0530

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,6 +1,6 @@
 import psycopg2
 try:
-    conn = psycopg2.connect("postgresql://postgres.ivajdrrrjktuarovumbd:ryoiki_tenkai__nehan_no_roka@aws-1-ap-southeast-1.pooler.supabase.com:6543/postgres")
+    conn = psycopg2.connect("postgresql://postgres.ivajdrrrjktuarovumbd:ryoiki_tenkai__nehan_wa_roka@aws-1-ap-southeast-1.pooler.supabase.com:6543/postgres")
     print("Connection successful!")
     conn.close()
 except Exception as e:
